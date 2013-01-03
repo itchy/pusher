6cee3606a24e67efef9cc1b64513f5326677eafb
Scott Johnson <7.scott.j@gmail.com>
2013-01-03 15:35:21 -0600

    create initial rails application using rspec and mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SharedBin::Application.config.secret_token = '34b7d3016cc009db399f4b992d0c6490009525135b5daa062465ade621dc82ea8ae00dd6d0f3a8b09090f012afdcebcef45a5e2b8904c545fcf13855504af03f'
