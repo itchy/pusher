fb4dafb589c12a8b671be62f741bbf8ce95b986d
Scott Johnson <7.scott.j@gmail.com>
2013-01-03 22:42:46 -0600

    add format for topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,6 +2,9 @@ class TopicsController < ApplicationController
   def create
     topic = Topic.new(params[:topic])
     topic.save
-    redirect_to topic.chat
+    respond_to do |format|
+      format.html { redirect_to topic.chat }
+      format.js { render :text => "This is what you get with js"}
+    end
   end
 end
