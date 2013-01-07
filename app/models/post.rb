dbfcdd80b4cdff0a54cda5dae0e95919905aca83
Scott Johnson <7.scott.j@gmail.com>
2013-01-07 15:46:29 -0600

    getting weird pg err, tryingout master branch

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :topic_id
+  belongs_to :topic
+  
+  # validates :content , :presence=> true, :length => { :minimum => 3 }
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,11 +2,30 @@ class Topic < ActiveRecord::Base
   attr_accessor :new
   attr_accessible :chat_id, :title # :rating, :status
   belongs_to :chat
+  has_many :posts
+  
+  # scope :active_posts, :conditions => ["posts.active = 1"]
+  
   validates :title, :presence => true, :length => { :minimum => 3 }
   validate :validate_chat_existance
+  # validates :status, :inclusion => { :in => [0,1,2] }
+  # validates_uniqueness_of :status, :scope => :chat_id,  :if => Proc.new { |topic| topic.active? }
   
+   
   def new?
     @new
+  end
+  
+  def open?
+    status == 0
+  end
+  
+  def active?
+    status == 1
+  end
+  
+  def closed?
+    status == 2
   end
   
   private
